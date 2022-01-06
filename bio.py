cd8cd607f358105dfaeadb61725e4e44f5ae07cd
Aida Pourshirazi <39318943+Aidapour@users.noreply.github.com>
2022-01-05 20:57:23 -0800

    Add files via upload

---
+++ bio.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3.9
+
+name = input ( "what is your name? ")
+color = input("what is your fav color? ")
+age = int(input("How old are you today? "))
+# print (name, end=" ")
+# print ("is " + str(age) + " years old")
+# print ("and love the color " + color + ".")
+
+print (name,  "is " + str(age) + " years old", "and love the color " + color + "." )
+
+nam = "Aida"
+print("Hey, %s!" % nam)
